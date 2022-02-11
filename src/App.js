3d66e226dc4dba2e115b7e3f1d3bbcaa732826b7
dbuchbut <dbuchbut@outbrain.com>
2022-02-11 08:57:21 +0200

    initial css grid

--- src/App.js
+++ src/App.js
@@ -2,31 +2,31 @@ import React, { Component } from "react"
 import logo from "./logo.svg"
 import "./App.css"
 
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { loading: false, msg: null }
-  }
+class Grid extends Component {
+  empty = {}
+  black = {}
+  white = {}
+  data = []
 
-  handleClick = api => e => {
-    e.preventDefault()
-
-    this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
+  initializeGrid() {
+    for (let r = 0; r < 3; r++) {
+      this.data.push([])
+      for (let c = 0; c < 3; c++) {
+        this.data[r][c] = this.empty
+      }
+    }
   }
 
   render() {
-    const { loading, msg } = this.state
+    const items = []
+    for (let r = 0; r < 3; r++) {
+      for (let c = 0; c < 3; c++) {
+        items.push(<div className="Grid-cell">{r + ':' + c}</div>)
+      }
+    }
 
     return (
-      <p>
-        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
-        <br />
-        <span>{msg}</span>
-      </p>
+      <div className="Grid">{items}</div>
     )
   }
 }
@@ -38,14 +38,9 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hello School of FrontendXX
+            Play X Mix Drix
           </p>
-          <div className="XMixGrid">
-            123
-            456
-            789
-          </div>
-          <LambdaDemo />
+          <Grid />
         </header>
       </div>
     )
